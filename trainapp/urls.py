bf054734dbb009f90f8275327e071c2f7d356755
Sebastian D'Asta <sebadasta@gmail.com>
2022-02-17 17:14:17 +0000

    added params to test html

--- trainapp/urls.py
+++ trainapp/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
+#path('path', method.func, name='html file')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('test', views.test, name ='test'),
 ]
--- trainapp/views.py
+++ trainapp/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 
 def index(request):
    return render(request, "index.html")
+
+def test(request):
+
+   testlist = ['asd','1q','dede']
+   return render(request, "test.html", {"testlist": testlist})
