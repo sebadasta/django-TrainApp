3a12775255687c252780ed24f11af55c40908946
Seba <sebadasta@gmail.com>
2022-10-16 19:16:19 +0100

    small test with Scheduler

--- scheduler.py
+++ scheduler.py
@@ -3,14 +3,14 @@ from trainapp.functions import *
 
 sched = BackgroundScheduler(timezone="Europe/Dublin")
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-  print('This job is run every one minutes.')
+#@sched.scheduled_job('interval', minutes=1)
+#def timed_job():
+#  print('This job is run every one minutes.')
 
-@sched.scheduled_job('cron',id="job_1", day_of_week='mon-sun', hour='8-23', minute='0/5')
+@sched.scheduled_job('cron',id="job_1", day_of_week='mon-sun', hour='8-23', minute='0/10')
 def scheduled_job():
-  #checkDartIssues()
-  print('This job is run on 0/5 8-23 * * 1-7')
+  checkDartIssues()
+  print('This job is run on 0/10 8-23 * * 1-7')
 
 
 
